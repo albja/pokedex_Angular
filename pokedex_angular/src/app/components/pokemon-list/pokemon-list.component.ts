e5bfebf119ff199a8d3a46fc183bd1d084f68b8b
Albja <alban.jaunet@sncf.fr>
2023-04-06 11:46:20 +0200

    it's fine , there are bugs

--- pokedex_angular/src/app/components/pokemon-list/pokemon-list.component.ts
+++ pokedex_angular/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/model/pokemon.model';
+
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
+@Input() pokemonData = Pokemon[];
+
 export class PokemonListComponent {
 
 }
--- pokedex_angular/src/app/pages/pokedex-page/pokedex-page.component.ts
+++ pokedex_angular/src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { PokedexService } from 'src/app/pokedex.service';
+import { Pokemon } from 'src/model/pokemon.model';
 @Component({
   selector: 'app-pokedex-page',
   templateUrl: './pokedex-page.component.html',
   styleUrls: ['./pokedex-page.component.css']
 })
 export class PokedexPageComponent {
+  pokemon: Pokemon[];
+  PokedexService: any;
+
+
+  constructor(public pokedexService: PokedexService) {
+
+    this.pokemon = this.PokedexService.getArrayPokemon()
+  }
 
 }
